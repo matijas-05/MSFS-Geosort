d3590487c2ee03c77216d032fabcb019a253f5dc
Mateusz Wojtyna <31010630+matijas-05@users.noreply.github.com>
2020-11-13 23:47:24 +0100

    Started project from scratch using WPF instead of Windows Forms

--- Geosort/Geosort/Properties/Resources.Designer.cs
+++ Geosort/Geosort/Properties/Resources.Designer.cs
@@ -8,10 +8,9 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
+
 namespace Geosort.Properties
 {
-
-
 	/// <summary>
 	///   A strongly-typed resource class, for looking up localized strings, etc.
 	/// </summary>
